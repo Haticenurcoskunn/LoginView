874fc8d204f60e352460fd46336ac82b06db724d
Hatice Nur <70744158+Haticenurcoskunn@users.noreply.github.com>
2024-06-27 19:19:23 +0300

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by nur on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
